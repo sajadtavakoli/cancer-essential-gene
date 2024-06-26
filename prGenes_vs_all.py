e1cff5f3c13592984f1447a78654bbf4548051c8
Sajad Tavakoli <41271921+sajadtavakoli@users.noreply.github.com>
2024-06-26 17:25:37 +0200

    the order is correct now

--- prGenes_vs_all.py
+++ prGenes_vs_all.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 from scipy.stats import pearsonr
 
-dir = './data/'
+dir = '/work3/sajata/ACB-course/cancer-essential-gene/data/'
 
 scores_path = dir+'Achilles_v2.11_training_phase3.gct'
 copy_path = dir+'CCLE_copynumber_training_phase3.gct'
@@ -53,35 +53,105 @@ mut_pr = mut_all[mut_all['Description'].isin(name_pr)].sort_values(by='Descripti
 # correlation between
 # shape: (#pr_genes, #all_genes)
 # 
-corr_exp_ess = {}
-for name1 in name_pr: 
-    temp = []
-    ess = ess_pr.loc[ess_pr['Description']==name1].to_numpy()
-    ess = ess.reshape(-1)[1:]
-    for name2 in common_genes:
-        exp = exp_all.loc[exp_all['Description']==name2].to_numpy()
-        exp = exp.reshape(-1)[1:]
-        corr, p_value = pearsonr(ess, exp)
-        temp.extend([corr, p_value])
-    temp = np.array(temp)
-    corr_exp_ess[name1] = temp
+import time
+common_genes = list(ess_all['Description']) # updated -> bug fixed (sorted now)
 
 
-corr_exp_exp = {}
-for name1 in name_pr: 
-    temp = []
+sajad
+###
+# -> calculating correlations and p-values and save in memeory 
+###
+
+counter = 0
+corr_exp_ess, corr_exp_exp = {}, {}
+for name1 in name_pr:
+    t1 = time.time()
+    
+    #temp_ess_exp = []
+    ess1 = ess_pr.loc[ess_pr['Description']==name1].to_numpy()
+    ess1 = ess1.reshape(-1)[1:]
+    
+    #temp_exp_exp = []
     exp1 = exp_pr.loc[exp_pr['Description']==name1].to_numpy()
     exp1 = exp1.reshape(-1)[1:]
+
+    temp = []
     for name2 in common_genes:
         exp2 = exp_all.loc[exp_all['Description']==name2].to_numpy()
         exp2 = exp2.reshape(-1)[1:]
-        corr, p_value = pearsonr(exp1, exp2)
-        temp.extend([corr, p_value]) ### BUG BUG BUG
+
+        corr_ess_exp, p_value_ess_exp = pearsonr(ess1, exp2)
+        #temp_ess_exp.append([corr_ess_exp, p_value_ess_exp])
+
+        corr_exp_exp, p_value_exp_exp = pearsonr(exp1, exp2)
+        temp.append([corr_ess_exp, p_value_ess_exp, corr_exp_exp, p_value_exp_exp])
+        
+    
+    #temp_ess_exp = np.array(temp_ess_exp)
+    #temp_exp_exp = np.array(temp_exp_exp) 
+    #np.save(dir+name1+'corr_ess_exp', temp_ess_exp)
+    #np.save(dir+name1+'corr_exp_exp', temp_exp_exp)
+    #corr_exp_ess[name1] = temp_ess_exp
+    #corr_exp_exp[name1] = temp_exp_exp
+    
     temp = np.array(temp)
-    corr_exp_exp[name1] = temp
+    np.save(dir+'corr/'+name1, temp)
 
-ali
+    t2 = time.time()
+    counter+=1
+    print(counter, 'priorotized gene:', name1) 
+    print(f'time: {t2-t1}\n')
+    print('-'*50)
 
+
+sajad
+###
+# -> creating image and labels for training and saving in memory
+###
+import joblib as gb
+paths = gb.load(dir+'corr/'+'*.npy')
+loaded_names = []
+cell_lines = ess_all.keys().to_list()[1:]
+x, y = [], []
+labels = {}
+for i, path in enumerate(paths):
+    gene_name = path.split('.')[0]
+    if gene_name in loaded_names: 
+        continue
+    loaded_names.append(gene_name)
+    corr = np.load(path)
+    img = np.zeros((8, len(common_genes)), dtype=np.float32)
+    
+    for j, cell in enumerate(cell_lines):
+        label = ess_pr[ess_pr['Description']==gene_name][cell].values[0]
+        exp_all_genes = exp_all[cell].to_numpy()   
+        exp_target_gene = np.array([exp_pr[exp_pr['Description']==gene_name][cell].values[0]] * len(common_genes))
+        cop_all_genes = cop_all[cell].to_numpy()  
+        cop_target_gene = cop_pr[cop_pr['Description']==gene_name][cell].values[0]
+        cop_target_gen = np.array([cop_target_gene]*len(common_genes))
+        img[:4, :] = corr.copy() 
+        img[4, :] = exp_all_genes
+        img[5, :] = exp_target_gene
+        img[6, :] = cop_all_genes
+        img[7, :] = cop_target_gen
+        np.save(dir+'imgs/'+f'img-f{gene_name}-{cell}', img)
+        labels[gene_name+'-'+cell] = label
+        break 
+    break 
+
+gb.dump('labels.joblib', labels)
+
+
+sajad
+
+
+
+     
+
+    
+
+
+sajad
 cell_lines = ess_all.keys().to_list()[1:]
 x, y = [], []
 for i, gene_name in enumerate(name_pr):
